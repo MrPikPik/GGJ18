c09124dd656437d1d8384c15913698e1bf1d1f62
Christoph Seegerer <christoph.seegerer@googlemail.com>
2018-02-04 14:50:15 +0100

    Collision Updates: Box > Capsule, Terrain Backdrop erweitert

--- Assets/Scripts/WorldRollOffEditor.cs
+++ Assets/Scripts/WorldRollOffEditor.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [ExecuteInEditMode]
 public class WorldRollOffEditor : MonoBehaviour {
-    [Range(-0.01f, 0.01f)]
+    [Range(-10.0f, 10.0f)]
     public float XRollOff = 0.0f;
 
-    [Range(-0.01f, 0.01f)]
+    [Range(-10.0f, 10.0f)]
     public float YRollOff = 0.0f;
+    [Range(1.0f, 10.0f)]
+    public float XScale = 1.0f;
+
+    [Range(1.0f, 10.0f)]
+    public float YScale = 1.0f;
 
 
-	void Update () {
-        Shader.SetGlobalFloat("curv", XRollOff);
-        Shader.SetGlobalFloat("curv2", YRollOff);
+    void Update () {
+        Shader.SetGlobalFloat("curv", XRollOff / 1000);
+        Shader.SetGlobalFloat("curv2", YRollOff / 1000);
+        Shader.SetGlobalFloat("sclX", XScale);
+        Shader.SetGlobalFloat("sclY", YScale);
     }
 }
